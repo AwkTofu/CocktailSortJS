18c238210b3b840cbbc6ca2270e1a1e45ca674c0
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-08-13 16:46:33 -0400

    Finished Cocktail Sort

--- cocktail_sort.js
+++ cocktail_sort.js
@@ -1,5 +1,46 @@
 let testArr = [5, 1, 4, 2, 8, 0, 2]
 
 function cocktailSort(arr) {
+	let start = 0, end = arr.length, swapped = true;
 
+	console.log(start, end, swapped);
+
+	while (swapped) {
+		swapped = false;
+
+		for (let i = start; i < end - 1; i++)
+		{
+			if (arr[i] > arr[i+1]) {
+				let temp = arr[i];
+				arr[i] = arr[i+1];
+				arr[i+1] = temp;
+				swapped = true;
+			}
+		}
+
+		end--;
+
+		if (!swapped)
+		{
+			break;
+		}
+
+		swapped = false;
+
+		for (let i = end - 1; i > start; i--)
+		{
+			if (arr[i - 1] > arr[i])
+			{
+				let temp = arr[i];
+				arr[i] = arr[i - 1];
+				arr[i - 1] = temp;
+				swapped = true;
+			}
+		}
+	}
+
+	return arr;
 }
+
+console.log("Before: ", testArr)
+console.log(cocktailSort(testArr));
